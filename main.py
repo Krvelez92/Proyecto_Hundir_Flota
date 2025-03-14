3854d9e9174d0d1fd35ae9189f15ae809106f712
Krvelez92 <karlarojasvelez92@gmail.com>
2025-03-14 20:08:21 +0100

    Archivos Codigos

---
+++ main.py
@@ -0,0 +1,25 @@
+##########################################
+#####          MAIN                  #####
+##########################################
+
+import utils as u
+import time
+
+print(
+'¡Bienvenido a Hundir la Flota! \n\nPrepárate para una emocionante batalla naval donde la estrategia y la puntería serán clave para la victoria.\nEn este juego, tendrás que localizar y hundir la flota enemiga antes de que tu oponente hunda la tuya. \nReglas del juego: \n\n- Cada jugador tiene una cuadrícula (10X10) donde coloca sus barcos (6) en posiciones estratégicas. \n- Los barcos pueden ocupar varias casillas y pueden estar dispuestos horizontal o verticalmente.\n- En cada turno, podrás disparar a una coordenada de la cuadrícula enemiga.\n- Si aciertas en una casilla ocupada por un barco enemigo, será un ¡impacto!. Si fallas, será agua.\n- Gana el jugador que logre hundir todos los barcos del oponente primero.\n\n¿Listo para la batalla?\n\n')
+
+
+print("        |    |    |        ")
+print("       )_)  )_)  )_)       ")
+print("      )___))___))___)\\    ")
+print("     )____)____)_____)\\   ")
+print("   _____|____|____|____\\__ ")
+print("   \\                   /   ")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n")
+print('Iniciemos!!')
+time.sleep(3)
+
+flota = {2:3}
+
+tablero_user, tablero_enemigo, tablero_enemigo_v, flota_usuario, flota_enemigo = u.iniciar_juego(flota)
+
---
+++ utils.py
@@ -0,0 +1,404 @@
+##########################################
+#####          LIBRERIAS             #####
+##########################################
+
+import numpy as np
+import random
+import time
+
+##########################################
+#####          FUNCIONES             #####
+##########################################
+
+''' 
+Resumen:
+ - crear_tablero(tamaño:tuple=(10,10))
+ - crear_barco(eslora:int)
+ - crear_barco_aleatorio(eslora:int)
+ - colocar_barco(barco:list, tablero, eslora:int, flota_barcos:list, funcion)
+ - llenar_tablero_user(flota:dict, tablero)
+ - llenar_tablero_enemigo(flota:dict, tablero)
+ - imprimir(tablero_user, tablero_enemigo_v)
+ - disparar(casilla:tuple, tablero, tablero_v, flota:list)
+ - validacion_tablero(tablero)
+ - iniciar_juego(flota:dict={3:2, 2:3, 1:4})
+ - turno_user(tablero_enemigo, tablero_enemigo_v, tablero_user, flota_enemigo:list)
+ - turno_enemigo(tablero_user, tablero_enemigo_v, flota_user:list)
+'''
+#----------------------------------------------------------------------------------------------------------------------------------
+
+def crear_tablero(tamaño:tuple=(10,10)):
+    ''' 
+    Esta función crear los tablero iniciales para el juego.
+
+    Input:
+        tamaño: tupla
+
+    Output:
+        tablero: array
+    '''
+    tablero = np.full(tamaño, '_') 
+    return tablero
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+def crear_barco(eslora:int):
+    ''' 
+    Esta es una función que crea los barcos del usuario.
+
+    Input:
+        eslora: int
+    
+    Output:
+        barco: list
+    '''
+    # Input Usuario primera posicion del barco
+    barco = input(f'Ingresa la posición inicia de tu barco eslora {eslora}.\nRecuerda el formato a seguir: ejemplo --> 0,1').strip()
+    barco =  list(barco.split(','))
+    barco = [tuple([int(x) for x in barco])]
+
+    # Input Usuario orientacion del barco
+    posicion = input('Ingresa la orientación del barco. Los valores posibles son: Norte, Sur, Este y Oeste. Elige uno y escribelo.').lower().strip()
+    posicion = posicion.lower().strip()
+
+    # Rellenar barco segun su eslora.
+    while len(barco) < eslora:
+        fila, columna = barco[-1] 
+
+        # NORTE
+        if posicion == "norte":
+            fila = fila - 1
+            if fila >= 0 and fila <= 9:
+                barco.append((fila, columna))
+            else:
+                barco.clear()
+                print(f'Error la ubicación inicial del barco segun su eslora {eslora} y su orientacion esta fuera de tablero.')
+                barco = crear_barco(eslora)
+
+        # SUR        
+        elif posicion == "sur":
+            fila = fila + 1
+            if fila >= 0 and fila <= 9:
+                barco.append((fila, columna))
+            else:
+                barco.clear()
+                print(f'Error la ubicación inicial del barco segun su eslora {eslora} y su orientacion esta fuera de tablero.')
+                barco = crear_barco(eslora)
+        
+        # ESTE 
+        elif posicion == "este":
+            columna = columna + 1
+            if columna >= 0 and columna <= 9:
+                barco.append((fila, columna))
+            else:
+                barco.clear()
+                print(f'Error la ubicación inicial del barco segun su eslora {eslora} y su orientacion esta fuera de tablero.')
+                barco = crear_barco(eslora)
+        
+        # OESTE 
+        elif posicion == "oeste":
+            columna = columna - 1
+            if columna >= 0 and columna <= 9:
+                barco.append((fila, columna))
+            else:
+                barco.clear()
+                print(f'Error la ubicación inicial del barco segun su eslora {eslora} y su orientacion esta fuera de tablero.')
+                barco = crear_barco(eslora)            
+               
+        else:
+            barco.clear()
+            print('Esta orientación no existe')
+            barco = crear_barco(eslora)
+
+    return barco
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+def crear_barco_aleatorio(eslora:int):
+    fila = random.randint(0,9)
+    columna = random.randint(0,9)
+    barco_aleatorio = [(fila, columna)]
+
+    orientacion = random.choice(['norte', 'sur', 'este', 'oeste'])
+
+    while len(barco_aleatorio) < eslora:
+        # NORTE
+        if orientacion == "norte":
+            fila = fila - 1
+            if fila >= 0 and fila <= 9:
+                barco_aleatorio.append((fila, columna))
+            else:
+                barco_aleatorio.clear()
+                barco_aleatorio = crear_barco_aleatorio(eslora)   
+
+        # SUR
+        elif orientacion == "sur":
+            fila = fila + 1
+            if fila >= 0 and fila <= 9:
+                barco_aleatorio.append((fila, columna))
+            else:
+                barco_aleatorio.clear()
+                barco_aleatorio = crear_barco_aleatorio(eslora)  
+
+        # ESTE
+        elif orientacion == "este":
+            columna = columna + 1
+            if columna >= 0 and columna <= 9:
+                barco_aleatorio.append((fila, columna))
+            else:
+                barco_aleatorio.clear()
+                barco_aleatorio = crear_barco_aleatorio(eslora) 
+
+        else:
+            columna = columna - 1
+            if columna >= 0 and columna <= 9:
+                barco_aleatorio.append((fila, columna))
+            else:
+                barco_aleatorio.clear()
+                barco_aleatorio = crear_barco_aleatorio(eslora) 
+
+    return barco_aleatorio
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+def colocar_barco(barco:list, tablero, eslora:int, flota_barcos:list, funcion):
+    ''' 
+    Esta es una función que coloca los barcos en el tablero.
+
+    Input:
+        barco: list
+    
+    Output:
+        tablero: array
+    '''
+    #Validamos primero si las casillas ya existen en el tablero
+    for casilla in barco:
+        if tablero[casilla] == '_':
+            continue
+        else:
+            print(f'El barco, se superpone con otro barco ya creado, vuelve a intertarlo.')
+            flota_barcos.remove(barco)
+            nuevo_barco = funcion(eslora)
+            flota_barcos.append(nuevo_barco)
+            return colocar_barco(nuevo_barco, tablero, eslora, flota_barcos, funcion)
+    
+    #Rellenamos la casillas con los nuevos barcos
+    for casilla in barco:    
+        tablero[casilla] = 'O'   
+    return tablero
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+def llenar_tablero_user(flota:dict, tablero):
+    ''' 
+    Esta es una función llena todo el tablero con la flota dada.
+
+    Input:
+        flota: dict
+    
+    Output:
+        flota_barcos: list
+    '''
+    flota_barcos = []
+    for num, eslora in flota.items():
+        for rep in range(num):
+            barco = crear_barco(eslora)
+            flota_barcos.append(barco)
+            colocar_barco(barco, tablero, eslora, flota_barcos, crear_barco)
+            print(flota_barcos)
+        
+    return flota_barcos
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+def llenar_tablero_enemigo(flota:dict, tablero):
+    ''' 
+    Esta es una función llena todo el tablero con la flota dada.
+
+    Input:
+        flota: dict
+    
+    Output:
+        flota_barcos: list
+    '''
+    flota_barcos = []
+    for num, eslora in flota.items():
+        for rep in range(num):
+            barco = crear_barco_aleatorio(eslora)
+            flota_barcos.append(barco)
+            colocar_barco(barco, tablero, eslora, flota_barcos, crear_barco_aleatorio)
+        
+    return flota_barcos
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+def imprimir(tablero_user, tablero_enemigo_v):
+    ''' 
+    Esta es una función imprime los tableros del usuario y del enemigo.
+
+    Input:
+        tablero_user: array
+        tablero_enemigo_v: array
+    
+    Output:
+        None   
+    '''
+    print('\t\t\tHundir la Flota')
+    print('*'*65)
+    print('\n')
+    print(f"{'':<2} {'User':^15} {'':<15} {'Enemigo':^30}")  
+    print('-' * 24 + ' '*15 + '-' * 21)  
+    columnas = "  " + " ".join(str(i+1) for i in range(10))
+    print(" " + columnas + " " * 14 + columnas)
+
+    for i in range(len(tablero_user)): 
+        fila_usuario = f"{i+1:<2} " + " ".join(tablero_user[i]) 
+        fila2 = " ".join(tablero_enemigo_v[i])
+        print(f'{fila_usuario:<35}    {fila2:<35}')  
+
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+def disparar(casilla:tuple, tablero, tablero_v, flota:list):
+    ''' 
+    Esta es una función que dispara en los tableros tanto para el usuario como la maquina.
+
+    Input:
+        casilla: tuple
+        tablero: array
+        tablero_v: array
+        flota: list
+    Output:
+        valor: str
+    '''
+    try:
+        tablero[casilla] in tablero
+
+        #If de tocado tiene una validacion para saber si se ha hundido.
+        if tablero[casilla] == 'O':
+            valor = 'X'
+            tablero[casilla] = valor
+            tablero_v[casilla] = valor
+            for barco in flota:
+                for j in barco:
+                    if j == casilla:
+                        lista = []
+                        for parte in barco:
+                            if tablero[parte] == 'X':
+                                lista.append('X')
+                        for parte in barco:
+                            if len(lista) == len(barco):
+                                valor = 'Z'
+                                tablero[parte] = valor
+                                tablero_v[parte] = valor
+            if tablero[casilla] == 'X':
+                print('Tocado, vuelve a disparar.')
+            else:            
+                print('Hundido, vuelve a disparar.')
+
+        elif tablero[casilla] == 'X':
+            valor = "¡OH NO! Ya habias disparado aquí, pierdes tu turno."    
+
+        elif tablero[casilla] == 'Z':
+            valor = "¡OH NO! Ya habias disparado aquí, pierdes tu turno."
+
+        elif tablero[casilla] == 'A':
+            valor = "¡OH NO! Ya habias disparado aquí, pierdes tu turno."
+
+        else:
+            print('Agua')
+            valor = 'A'
+            tablero[casilla]  = valor
+            tablero_v[casilla]  = valor          
+    except:
+        valor = "¡OH NO! Te has salido del tablero, pierdes tu turno."
+    return valor
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+def validacion_tablero(tablero):
+    ''' 
+    Funcion para validar si sigue el juego o no.
+
+    Input:
+        tablero: array
+    
+    Output:
+        bool
+    '''
+    for lis in tablero:
+        if 'O' in lis:
+            return True
+    return False
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+def iniciar_juego(flota:dict={3:2, 2:3, 1:4}):
+    ''' 
+    Esta es una función para iniciar el juego donde ser crean los tablero del usuario y del enemigo.
+
+    Input:
+        flota: dict
+    
+    Output:
+        None   
+    '''
+    tablero_user = crear_tablero()
+    tablero_enemigo = crear_tablero()
+    tablero_enemigo_v = crear_tablero()
+    print('Creando Tablero Usuario')
+    print('*'*65)
+    flota_usuario = llenar_tablero_user(flota, tablero_user)
+    print('\n')
+    print('Creando Tablero Enemigo')
+    print('*'*65)
+    flota_enemigo = llenar_tablero_enemigo(flota, tablero_enemigo)
+    print('\n')
+    time.sleep(2)
+    imprimir(tablero_user, tablero_enemigo_v), 
+    return tablero_user, tablero_enemigo, tablero_enemigo_v, flota_usuario, flota_enemigo
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+def turno_user(tablero_enemigo, tablero_enemigo_v, tablero_user, flota_enemigo:list):
+    ''' 
+    Funcion que genera el disparo del usuario
+
+    Input:
+        tablero_enemigo: array
+        tablero_enemigo_v: array
+        flota_enemigo: list
+    
+    Output:
+        disparo: str
+    '''
+    casilla_disparo = input('Es tu turno, elige una casilla para disparar ¡Vamos a por el enemigo!\nRecuerda el formato a seguir: ejemplo --> 0,1').strip()
+    casilla =  list(casilla_disparo.split(','))
+    casilla = tuple([int(x) for x in casilla])
+    disparo = disparar(casilla, tablero_enemigo, tablero_enemigo_v, flota_enemigo)
+    imprimir(tablero_user, tablero_enemigo_v)
+    print('\n')
+    print('-'*65)
+    return disparo
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+def turno_enemigo(tablero_user, tablero_enemigo_v, flota_user:list):
+    ''' 
+    Funcion que genera el disparo del enemigo
+
+    Input:
+        tablero_user: array
+        flota_user: list
+    
+    Output:
+        disparo: str
+    '''
+    fila = random.randint(0,9)
+    columna = random.randint(0,9)
+    casilla = (fila, columna)
+    disparo = disparar(casilla, tablero_user, tablero_user, flota_user)
+    imprimir(tablero_user, tablero_enemigo_v)
+    print('\n')
+    print('-'*66)
+    return disparo
